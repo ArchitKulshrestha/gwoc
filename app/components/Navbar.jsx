01c5980ad797c75bac3be842add1b3eab38349f6
Archit <archit499kul@gmail.com>
2023-12-31 02:01:53 +0530

    Refactor Navbar component styling

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -10,20 +10,20 @@ import { IoMdClose } from "react-icons/io";
 const Navbar = () => {
   const [showMenu, setShowMenu] = useState(false);
   return (
-    <nav className="fixed px-6 lg:px-20 z-50 backdrop-blur-[2px]   bg-background-100/60 w-full h-[70px] flex justify-between items-center border-b-2 border-background-100 shadow-md">
+    <motion.nav
+      className="fixed px-6 lg:px-20 z-50 backdrop-blur-[2px]  bg-background-100/60 w-full h-[70px] flex justify-between items-center border-b-2 border-background-100 shadow-md"
+      initial={{ y: -100 }}
+      animate={{ y: 0, transition: { duration: 0.4 } }}>
       <Link href={"/"} className="text-text-700 text-xl font-bold uppercase ">
-        <motion.p
-          initial={{ opacity: 0, scale: 0.5 }}
-          animate={{ opacity: 1, scale: 1 }}
-          transition={{ duration: 0.4 }}>
+        <p className="hover:scale-105 transition-all duration-[300ms]">
           Tarang &nbsp;
           <span className="md:inline-block p-0 hidden">Bombale</span>
-        </motion.p>
+        </p>
       </Link>
       <ul className="hidden lg:flex h-full flex-row gap-4 items-center">
         {NavLinks.map((link) => (
           <li
-            className=" hover:scale-105 text-text-700 hover:text-text-800"
+            className="  text-text-700 hover:text-text-800 hover:scale-105 transition-all duration-200"
             key={link.id}
             id={link.id}>
             <Link
@@ -57,7 +57,7 @@ const Navbar = () => {
           </ul>
         </div>
       )}
-    </nav>
+    </motion.nav>
   );
 };
 
